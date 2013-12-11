8659bee02743be0975d13935328c65bc71a5411b
Paul McKee <paul.mckee.aus@gmail.com>
2013-12-11 23:36:12 +1000

    cleanedup video and chat

--- server/server.js
+++ server/server.js
@@ -26,16 +26,16 @@ var http  = require("http"),
 // console.log("Cert:", caCertFilePath);
 
 
- var options = {
-   key: fs.readFileSync('server/ssl/pauls-ssl-key.pem'),
-   cert: fs.readFileSync('server/ssl/pauls-ssl-cert.pem'),
+ // var options = {
+ //   key: fs.readFileSync('server/ssl/pauls-ssl-key.pem'),
+ //   cert: fs.readFileSync('server/ssl/pauls-ssl-cert.pem'),
    
- };
+ // };
 
-// var options = {
-//   key: fs.readFileSync('server/secret/private.key'),
-//   cert: fs.readFileSync('server/secret/self.crt')
-// };
+var options = {
+  key: fs.readFileSync('server/secret/private.key'),
+  cert: fs.readFileSync('server/secret/self.crt')
+};
 
 //var options = {
 //    // Important: the following crt and key files are insecure
@@ -55,30 +55,8 @@ var http  = require("http"),
 //   res.end("hello world\n");
 // }).listen(8000);
 
-var server = http.createServer(function (request, response) {
-  console.log(request);
-  console.log(request.headers.host);
-
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-// console.log("Server running at http://127.0.0.1:8000/  Hello World");
-// console.log("Server running at https://127.0.0.1:8080/");
-
-console.log("Server running at http: 8000  Hello World");
-console.log("Server running at https: :8080");
-
-
-
-//https.createServer(function(req, res) {
-https.createServer(options, function (req, res) {
-    
-    var path_Html_Public = path.join("..","public");
+var processRequest = function(req, res){
+   var path_Html_Public = path.join("..","public");
     
    
     var filename = path.basename(req.url) || "index.html",
@@ -116,8 +94,34 @@ https.createServer(options, function (req, res) {
        console.log("extension Not mapped: " + ext);
        res.writeHead(404);
         res.end();
-    }
+    } 
+  };
+
+var server = http.createServer(function (req,res) {
+  console.log(req);
+  console.log(req.headers.host);
+  processRequest(req,res);
+
+  //response.writeHead(200, {"Content-Type": "text/plain"});
+  //response.end("Hello World\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+console.log("Server running at http: 8000");
+
+// Put a friendly message on the terminal
+// console.log("Server running at http://127.0.0.1:8000/  Hello World");
+// console.log("Server running at https://127.0.0.1:8080/");
+
+//https.createServer(function(req, res) {
+https.createServer(options, function (req, res) {
+    processRequest(req,res);
+  
 }).listen(8080);
+
+console.log("Server running at https: :8080");
+
 
 function getFile(localPath, mimeType, res) {
   // read the file in and return it, or return a 500 if it can't be read
@@ -135,7 +139,55 @@ function getFile(localPath, mimeType, res) {
 }
 
 
-// https.createServer(options, function (req, res) {
-//   res.writeHead(200);
-//   res.end("hello world\n");
-// }).listen(8000);
+
+<!-- Peer 2 peer -->
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket){
+
+  // convenience function to log server messages on the client
+  function log(){
+    var array = [">>> Message from server: "];
+    for (var i = 0; i < arguments.length; i++) {
+      array.push(arguments[i]);
+    }
+      socket.emit('log', array);
+  }
+
+
+  socket.on('chat', function (message) {
+    log('Got chat message:', message);
+    // for a real app, would be room only (not broadcast)
+    socket.broadcast.emit('chat', message);
+    log('Emit chat message:', message);
+ 
+  });
+
+  socket.on('message', function (message) {
+    log('Got message:', message);
+    // for a real app, would be room only (not broadcast)
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('create or join', function (room) {
+    var numClients = io.sockets.clients(room).length;
+
+    log('Room ' + room + ' has ' + numClients + ' client(s)');
+    log('Request to create or join room ' + room);
+
+    if (numClients === 0){
+      socket.join(room);
+      socket.emit('created', room);
+    } else if (numClients === 1) {
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room);
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+    socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
+    socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+
+  });
+
+});
